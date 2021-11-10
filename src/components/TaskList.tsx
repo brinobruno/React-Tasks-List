9b0500830d886cd0deaea16a96a83dd5f6a62ffa
Bruno Corrêa <brunosantos6ft@gmail.com>
2021-11-10 14:12:23 -0300

    :tada: desafio completo

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -24,14 +24,23 @@ export function TaskList() {
     }
 
     setTasks(oldState => [...oldState, newTask])
+    setNewTaskTitle('')
   }
 
-  function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+  function handleToggleTaskCompletion(id: number) {   // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newCompletion = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(newCompletion)
+
   }
 
-  function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+  function handleRemoveTask(id: number) {   // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id != id)
+
+    setTasks(filteredTasks)
   }
 
   return (
